3ed99d59726f27d0cac6ad94da088e016adfd73f
Guillaume Chainet <gchainet@student.42.fr>
2019-10-14 18:20:25 +0200

    Add method to set redirection table in IOAPIC

--- src/pic/src/ioapic.rs
+++ src/pic/src/ioapic.rs
@@ -10,37 +10,63 @@ pub struct IOApic {
 }
 
 impl IOApic {
-    const IOAPICREGSEL: u32 = 0x0;
-    const IOAPICWIN: u32 = 0x10;
-    const IOAPICID: u32 = 0x0;
-    const IOAPICVER: u32 = 0x1;
-    const IOAPICARB: u32 = 0x2;
+        const IOAPICREGSEL: u8 = 0x0;
+        const IOAPICWIN: u8 = 0x10;
 
-    pub unsafe fn new(io_apic_ptr: *const MADT_IOAPIC) -> IOApic {
-        let mut io_apic = IOApic {
-            id: (*io_apic_ptr).ioapic_id,
-            addr: (*io_apic_ptr).ioapic_addr,
-            int_base: (*io_apic_ptr).global_system_interrupt_base,
-            max_irq: 0
-        };
-        if let Err(error) = ALLOCATOR.id_map(io_apic.addr as usize, mem::frame::FRAME_SIZE) {
-            panic!("Unable to map IOAPIC {:?}", error);
+        const IOAPICID: u8 = 0x0;
+        const IOAPICVER: u8 = 0x1;
+        const IOAPICARB: u8 = 0x2;
+
+        pub unsafe fn new(io_apic_ptr: *const MADT_IOAPIC) -> IOApic {
+            let mut io_apic = IOApic {
+                id: (*io_apic_ptr).ioapic_id,
+                addr: (*io_apic_ptr).ioapic_addr,
+                int_base: (*io_apic_ptr).global_system_interrupt_base,
+                max_irq: 0
+            };
+            if let Err(error) = ALLOCATOR.id_map(io_apic.addr as usize, mem::frame::FRAME_SIZE) {
+                panic!("Unable to map IOAPIC {:?}", error);
+            }
+            let ver = io_apic.read_reg(Self::IOAPICVER);
+            io_apic.max_irq = (ver >> 16) & 0xff;
+            io_apic
         }
-        let ver = io_apic.read_reg(Self::IOAPICVER);
-        io_apic.max_irq = (ver >> 16) & 0xff;
-        io_apic
-    }
 
-    unsafe fn read_reg(&self, reg: u32) -> u32 {
-        let value: u32;
-        let mut selector: u32;
-        asm!("mov eax, [$0]" : "={eax}"(selector) : "r"(self.addr + Self::IOAPICREGSEL) :: "intel");
-        selector = (selector & !0xff) | reg as u32;
-        asm!("mov [ebx], ecx 
-            mov eax, [edx]"
-            : "={eax}"(value)
-            : "{ebx}"(self.addr + Self::IOAPICREGSEL), "{edx}"(self.addr + Self::IOAPICWIN), "{ecx}"(selector)
-            :: "intel", "volatile");
-        value
-    }
+        unsafe fn get_selector(&self, reg: u8) -> u32 {
+            let mut selector: u32;
+            asm!("mov eax, [$0]"
+                : "={eax}"(selector)
+                : "r"(self.addr + Self::IOAPICREGSEL as u32)
+                :: "intel");
+            (selector & !0xff) | reg as u32
+        }
+
+        unsafe fn read_reg(&self, reg: u8) -> u32 {
+            let value: u32;
+            let selector = self.get_selector(reg);
+            asm!("mov [ebx], ecx 
+                mov eax, [edx]"
+                : "={eax}"(value)
+                : "{ebx}"(self.addr + Self::IOAPICREGSEL as u32), 
+                "{ecx}"(selector),
+                "{edx}"(self.addr + Self::IOAPICWIN as u32)
+                :: "intel", "volatile");
+            value
+        }
+
+        unsafe fn write_reg(&self, reg: u8, value: u32) {
+            let selector = self.get_selector(reg);
+            asm!("mov [eax], ebx
+                mov [ecx], edx"
+                :: "{eax}"(self.addr + Self::IOAPICREGSEL as u32),
+                "{ebx}"(selector),
+                "{ecx}"(self.addr + Self::IOAPICWIN as u32),
+                "{edx}"(value)
+                :: "intel");
+        }
+
+        unsafe fn write_rtbl(&self, id: u8, value: u64) {
+            self.write_reg(id, (value & 0xffff) as u32);
+            self.write_reg(id + 1, (value >> 32) as u32);
+        }
 }
